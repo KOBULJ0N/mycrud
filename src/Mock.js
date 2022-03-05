a91d883ab4c8d1e9492e8a842fa7d75f616f2ce2
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2022-03-05 14:22:12 +0500

    something

--- src/Mock.js
+++ src/Mock.js
@@ -1,5 +1,5 @@
 export const Data = [
-  { id: 1, name: 'Qobiljon', age: 21 },
+  { id: 1, name: 'Elbek', age: 23 },
   { id: 2, name: 'DAvid', age: 20 },
   { id: 3, name: 'Dave', age: 22 },
   { id: 4, name: 'John', age: 23 },
