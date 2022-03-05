0fb5bfe078175f6b686d4fa604798000d803dc40
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2022-03-05 13:08:59 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Data from './Mock';
 
-function App() {
+export const App = () => {
+  const storage = localStorage.getItem('dataa');
+
+  const [item, setItem] = useState('');
+  const [select, setSelect] = useState(null);
+  const [title, setTitle] = useState('');
+  const [age, setAge] = useState('');
+
+  const [data, setData] = useState(storage ? JSON.parse(storage) : Data);
+
+
+  localStorage.setItem('dataa', JSON.stringify(data));
+
+  
+
+  /* --------------Delete-----------------*/
+
+  const onDelete = (ids) => {
+    const newData = data.filter((value) => value.id !== ids);
+    setData(newData);
+  };
+
+  /* --------------Delete-----------------*/
+
+  /* --------------Create-----------------*/
+
+  const getSave = () => {
+    const created = [
+      ...data,
+      {
+        id: data.length + 1,
+        name: item,
+        age: Math.floor(Math.random() * 100),
+      },
+    ];
+
+    item && setData(created);
+    setItem('');
+  };
+
+  /* --------------Create-----------------*/
+
+  /* --------------edit-----------------*/
+
+  const getEdit = (value) => {
+    setSelect(value.id);
+    setTitle(value.name);
+    setAge(value.age) 
+   
+  };
+
+  const saveEdit = () => {
+    const newEditData = data.map((value) =>
+      select == value.id ? { ...value, name: title, age: age } : value
+    );
+    setData(newEditData);
+    setSelect(null);
+  };
+  /* --------------edit-----------------*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div>
+        <table border='1' width={'50%'}>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Age</th>
+              <th>Name</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          {data.length === 0 ? (
+            <h3>There's no Data</h3>
+          ) : (
+            data.map((value) => (
+              <tbody key={value.id}>
+                <tr>
+                  <td>{value.id}</td>
+                  <td>
+                    {select == value.id ? (
+                      <input
+                        type='text'
+                        onChange={(e) => setAge(e.target.value)}
+                        value={age}
+                      />
+                    ) : (
+                      value.age
+                    )}
+                  </td>
+                  <td>
+                    {select == value.id ? (
+                      <input
+                        type='text'
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                      />
+                    ) : (
+                      value.name
+                    )}
+                  </td>
+                  <td>
+                    <button onClick={() => onDelete(value.id)}>delete</button>
 
+                    {select == value.id ? (
+                      <button onClick={saveEdit}>save</button>
+                    ) : (
+                      <button onClick={() => getEdit(value)}>edit</button>
+                    )}
+                  </td>
+                </tr>
+              </tbody>
+            ))
+          )}
+        </table>
+        <input
+          placeholder='write your data'
+          onChange={(e) => setItem(e.target.value)}
+          type='text'
+          value={item}
+        />
+        <button onClick={getSave}>save</button>
+      </div>
+
+      <>
+      
+      </>
+    </>
+  );
+};
 export default App;
